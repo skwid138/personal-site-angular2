febbf3b131f40a516dc9799bdc3195899e988725
Hunter Rancourt <skwid138@gmail.com>
2017-12-15 19:51:08 -0600

    setup angular material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// angular material design
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatTooltip } from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTooltip
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// hammer js works with angular material design
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
